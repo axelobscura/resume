29635119761c8257dc3f99d0599a5883cb0cf36c
axelinstrumental <aobscura@instrumental.net>
2024-07-17 14:40:08 -0600

    icons

--- src/app/professional-summary/page.tsx
+++ src/app/professional-summary/page.tsx
@@ -32,7 +32,7 @@ export default function ProfessionalSummary() {
                           <p>{job.rol}</p>
                       </Link>
                       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{job.client}</p>
-                      <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-yellow-600 dark:hover:bg-gray-700 dark:focus:ring-gray-100">
+                      <Link href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-yellow-600 dark:hover:bg-gray-700 dark:focus:ring-gray-100 transition-all duration-500">
                           Read more
                           <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
