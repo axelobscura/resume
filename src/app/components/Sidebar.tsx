42e89f1375df221a4d2b5f0ae311c0f1107fbb5b
axelinstrumental <aobscura@instrumental.net>
2024-07-17 10:58:11 -0600

    icons

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Image from 'next/image';
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -6,11 +7,21 @@ const Sidebar = () => {
   return (
     <div className="flex">
       <div
-        className={`bg-gray-900 text-white h-screen transition-all duration-300 z-10 flex items-center justify-between ${
+        className={`bg-gray-900 text-white h-screen transition-all duration-300 z-10 flex flex-col items-center justify-center ${
           isOpen? 'w-full' : 'w-0 overflow-hidden'
         }`}
       >
-        {/* Sidebar content */}
+        <div className='text-center flex flex-col items-center'>
+          <Image
+            src="/6198657.jpeg"
+            width={75}
+            height={75}
+            alt="Full Stack Developer in Mexico Axel Laurent Obscura Sarzotti"
+            className='rounded-full mb-3'
+          />
+          <p className='text-center font-extrabold text-2xl'>Axel Laurent<br/>Obscura Sarzotti</p>
+          <p className='text-center font-thin text-gray-300 mt-3'>Sr. Full Stack Developer</p>
+        </div>
         <div className="flex flex-col items-center">
           <div className="mt-4">
             <a href="#" className="text-white hover:text-gray-300">
@@ -22,7 +33,6 @@ const Sidebar = () => {
               About
             </a>
           </div>
-          {/* Add more sidebar items here */}
         </div>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="grid grid-cols-[1fr_7fr] gap-0">
+      <div className="grid grid-cols-[1fr_5fr] gap-0">
         <div className="p-0">
           <Sidebar />
         </div>
-        <div className="flex flex-col items-center justify-center bg-gray-700 p-4">
-          <h1 className="m-1 mb-3 text-white text-8xl font-thin">Welcome</h1>
+        <div className="flex flex-col items-center justify-center p-4">
+          <h1 className="m-1 mb-3 text-white text-8xl font-thin">Welcome!</h1>
           <h2 className="pt-0 text-4xl mt-2 mb-4 font-bold text-gray-300">Axel Laurent Obscura Sarzotti</h2>
+          <h3 className="font-extrabold mt-0 mb-3 text-3xl text-gray-300">Senior Full Stack Developer</h3>
           <div className="flex icod m-1">
             <i className="devicon-javascript-plain m-2 text-3xl"></i>
             <i className="devicon-typescript-plain m-2 text-3xl"></i>
@@ -36,23 +37,26 @@ export default function Home() {
             <i className="devicon-zend-plain m-2 text-3xl"></i>
             <i className="devicon-mocha-plain m-2 text-3xl"></i>
             <i className="devicon-laravel-plain m-2 text-3xl"></i>
+            <i className="devicon-tailwindcss-original m-2 text-3xl"></i>
             <i className="devicon-less-plain-wordmark m-2 text-3xl"></i>
             <i className="devicon-mongodb-plain m-2 text-3xl"></i>
             <i className="devicon-mysql-plain m-2 text-3xl"></i>
             <i className="devicon-sass-original m-2 text-3xl"></i>
+            <i className="devicon-amazonwebservices-plain-wordmark m-2 text-3xl"></i>
+            <i className="devicon-azure-plain m-2 text-3xl"></i>
             <i className="devicon-ssh-plain m-2 text-3xl"></i>
           </div>
           <hr />
-          <h3 className="text-white font-extrabold mt-3 mb-3 text-3xl text-gray-300">Senior Full Stack Developer</h3>
-          <p className="text-center">
-            With 20 years of experience designing, developing and maintaining
+          
+          <p className="text-center mt-3">
+            With <strong>20 years of experience</strong> designing, developing and maintaining
             web and mobile applications
             <br />
             extensive experience creating multiple highly scalable applications{" "}
             <b>using different modern tech stacks.</b>
           </p>
           <Link href="/professional-summary">
-            <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-8 rounded mt-8">Professional Summary</button>
+            <button className="bg-yellow-400 hover:bg-gray-700 text-gray-900 uppercase font-bold py-3 px-8 mt-8">Professional Summary</button>
           </Link>
         </div>
       </div>
