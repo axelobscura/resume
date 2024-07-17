2d0c764f49097a4b10789b6c24f39fe08043f356
axelinstrumental <aobscura@instrumental.net>
2024-07-17 12:54:25 -0600

    icons

---
+++ src/app/api/portfolio/route.tsx
@@ -0,0 +1,216 @@
+import { NextResponse } from 'next/server';
+
+const works = [
+  {
+    "empresa": "Revenue river",
+    "rol": "Full Stack Developer",
+    "stack": ["ReactJS", "Ruby on Rails", "Tailwind", "Bootstrap", "Typescript", "React router", "Hasura", "GraphQl"],
+    "periodo": "2021-2023",
+    "client": "ASYMM - US Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "Baires",
+    "rol": "Full Stack Developer",
+    "stack": ["ReactJS", "Ruby on Rails", "Tailwind", "Bootstrap", "Typescript"],
+    "periodo": "2020-2021",
+    "client": "Baires - US Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "Asymm",
+    "rol": "Full Stack Developer",
+    "stack": ["ReactJS", "Ruby on Rails", "Tailwind", "Bootstrap", "Typescript", "React router", "Hasura", "GraphQl", "Docker", "Kubernets"],
+    "periodo": "2021-2022",
+    "client": "ASYMM - US Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "Delart",
+    "rol": "Full Stack Developer",
+    "stack": ["ReactJS", "Tailwind", "React router", "Hasura", "GraphQl"],
+    "periodo": "2021",
+    "client": "DRESSLER - US Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "Rings.io",
+    "rol": "Frontend Developer",
+    "stack": ["ReactJS", "Tailwind", "Salesforce API"],
+    "periodo": "2021",
+    "client": "SCREEN IT - Mexico Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "Ford Motors Company US",
+    "rol": "Frontend Developer",
+    "stack": ["ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2021",
+    "client": "US Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "INNOCEAN México",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2021",
+    "client": "US Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "Instituto Mexicano del Cemento y del Concreto A.C",
+    "rol": "Full Stack Developer",
+    "stack": ["ReactJS", "Tailwind", "React router", "Hasura", "GraphQl"],
+    "periodo": "2011 - 2021",
+    "client": "Mexico Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "Gobierno de la Ciudad de México - ADIP",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2020",
+    "client": "Mexico Government",
+    "responsabilidades": [
+      "Responsible of database architecture",
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "THRIVE MARKET",
+    "rol": "Frontend Developer",
+    "stack": ["NextJS"],
+    "periodo": "2020",
+    "client": "US Based Company",
+    "responsabilidades": [
+      "Responsible of database architecture",
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "TRYBU.MX",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "Wordpress", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2020",
+    "client": "US Based Company",
+    "responsabilidades": [
+      "Responsible of database architecture",
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "PASSUR",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "Wordpress", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2020",
+    "client": "US Based Company",
+    "responsabilidades": [
+      "Responsible of database architecture",
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS",
+      "Full Stack Development with React-Redux in multiple APP’s"
+    ]
+  },
+  {
+    "empresa": "AMGG",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "Wordpress", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2020",
+    "client": "Asociación Mexicana de Gerontología y Geriatría",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS"
+    ]
+  },
+  {
+    "empresa": "MUNICIPIO DE COATZINTLA",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "Wordpress", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2020",
+    "client": "Gobierno de la Veracruz",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS"
+    ]
+  },
+  {
+    "empresa": "SHINHAN BANK MÉXICO",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "Wordpress", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2015 - 2021",
+    "client": "Asociación Mexicana de Gerontología y Geriatría",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS"
+    ]
+  },
+  {
+    "empresa": "BHUVI IT SOLUTIONS",
+    "rol": "Full Stack Developer",
+    "stack": ["PHP", "MySQL", "Wordpress", "ReactJS", "Bootstrap", "React router", "Salesforce API"],
+    "periodo": "2016 - 2017",
+    "client": "US Based Company",
+    "responsabilidades": [
+      "Involved in documentation of the architecture and configuration details",
+      "Migrated existing systems to AWS using EC2 instances, auto-scaling, created VPN's, Security Groups, configuring a Load Balancers and creation of CloudFormation automation scripts",
+      "Co-ordinate with UX designers and develop static content using front end with HTML 5, CSS3 and React/Angular JS"
+    ]
+  }
+];
+ 
+export async function GET(request: Request) {
+  return NextResponse.json({ msg: works })
+}
--- src/app/professional-summary/page.tsx
+++ src/app/professional-summary/page.tsx
@@ -1,16 +1,37 @@
 "use client";
+import { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import Link from "next/link";
 
 export default function ProfessionalSummary() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    fetch(`/api/portfolio`)
+      .then((res) => res.json())
+      .then((data) => setResults(data.msg));
+  },[]);
+
+  console.log(results);
+
   return (
     <main className="flex min-h-screen flex-col">
       <div className="grid lg:grid-cols-[1fr_5fr] md:grid-cols-[2fr_5fr] sm:grid-cols-[3fr_5fr] gap-0">
         <div className="p-0">
           <Sidebar />
         </div>
-        <div className="flex flex-col items-center justify-center p-4">
-          <h1 className="text-4xl text-white uppercase">Professional Summary</h1>
+        <div className="flex flex-col items-center justify-center p-4 h-screen overflow-auto pt-20 pb-5">
+          {!results.length ? 
+            <h1 className="text-4xl text-white uppercase">Loading</h1>
+          : 
+            results.map((job:any) => {
+              return (
+                <div key={job} className="flex flex-col items-center justify-center p-4">
+                  <h2 className="text-2xl text-white uppercase">{job.empresa}</h2>
+                </div>
+              );
+            })
+          }
         </div>
       </div>
     </main>
