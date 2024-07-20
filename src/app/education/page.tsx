7dd973676e45689bf79a2479656018a38f814e57
axelinstrumental <aobscura@instrumental.net>
2024-07-20 08:33:21 -0600

    education

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,32 +1,78 @@
 "use client";
 import Sidebar from "../components/Sidebar";
 import Link from "next/link";
+import Image from 'next/image';
 
 export default function Education() {
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="grid lg:grid-cols-[1fr_5fr] md:grid-cols-[2fr_5fr] sm:grid-cols-[3fr_5fr] gap-0">
+      <div className="grid lg:grid-cols-[1fr_5fr] md:grid-cols-[2fr_5fr] sm:grid-cols-[3fr_5fr] gap-0 overflow-hidden">
         <div className="p-0">
           <Sidebar />
         </div>
-        <div className="flex flex-col justify-center p-10">
+        <div className="flex flex-col justify-center p-10 h-full overflow-auto">
           <h1 className="text-4xl text-white uppercase mt-5 mb-5 font-black text-center">Education</h1>
           <hr />
-          <h2 className="text-left pt-2 font-extrabold">BACHELOR’S DEGREE IN GRAPHIC & DIGITAL COMMUNICATIONS | WEB DEVELOPMENT</h2>
-          <h3>UNIVERSIDAD AUTÓNOMA METROPOLITANA</h3>
-          <p className="pb-2 text-2xs">YEAR OF COMPLETION: 2004</p>
-          <hr />
-          <h2 className="text-left pt-2 font-extrabold">MASTER’S DEGREE IN VISUAL ARTS / BRANCH WEB DESIGN AND DIGITAL DEVELOPMENT</h2>
-          <h3>UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO</h3>
-          <p className="pb-2 text-2sm">YEAR OF COMPLETION: 2008</p>
-          <hr />
-          <h2 className="text-left pt-2 font-extrabold">MASTER’S DEGREE IN SYSTEM ENGINEERING (IN PROCESS)</h2>
-          <h3>UNIVERSIDAD INTERNACIONAL DE LA RIOJA</h3>
-          <p className="pb-2 text-2sm">IN PROCESS: 2019</p>
-          <hr />
-          <h2 className="text-left pt-2 font-extrabold">FULL-STACK ENGINEER</h2>
-          <h3>CODE ACADEMY</h3>
-          <p className="pb-2 text-2sm">IN PROCESS: 2021 - 2022</p>
+
+          <Link href="#" className="flex flex-col w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-3">
+            <Image
+              src="/6198657.jpeg"
+              width={75}
+              height={75}
+              alt="Full Stack Developer in Mexico Axel Laurent Obscura Sarzotti"
+              className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
+            />
+            <div className="flex flex-col justify-between p-2 leading-normal">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">UNIVERSIDAD AUTÓNOMA METROPOLITANA</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">BACHELOR’S DEGREE IN GRAPHIC & DIGITAL COMMUNICATIONS<br/>WEB DESIGN AND DEVELOPMENT</h5>
+                <p className="text-sm">1999 - 2004, YEAR OF COMPLETION: 2004</p>
+            </div>
+          </Link>
+
+          <Link href="#" className="flex flex-col w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-3">
+            <Image
+              src="/6198657.jpeg"
+              width={75}
+              height={75}
+              alt="Full Stack Developer in Mexico Axel Laurent Obscura Sarzotti"
+              className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
+            />
+            <div className="flex flex-col justify-between p-2 leading-normal">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">UNIVERSIDAD NACIONAL AUTÓNOMA DE MÉXICO</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">MASTER’S DEGREE IN VISUAL ARTS<br/>BRANCH WEB DESIGN AND DIGITAL DEVELOPMENT</h5>
+                <p className="text-sm">2006 - 2008, YEAR OF COMPLETION: 2008</p>
+            </div>
+          </Link>
+
+          <Link href="#" className="flex flex-col w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-3">
+            <Image
+              src="/6198657.jpeg"
+              width={75}
+              height={75}
+              alt="Full Stack Developer in Mexico Axel Laurent Obscura Sarzotti"
+              className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
+            />
+            <div className="flex flex-col justify-between p-2 leading-normal">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">UNIVERSIDAD INTERNACIONAL DE LA RIOJA</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">MASTER’S DEGREE IN SYSTEM ENGINEERING (IN PROCESS)</h5>
+                <p className="text-sm">2019 - 2020, YEAR OF COMPLETION: 2020</p>
+            </div>
+          </Link>
+
+          <Link href="#" className="flex flex-col w-full items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-3">
+            <Image
+              src="/6198657.jpeg"
+              width={75}
+              height={75}
+              alt="Full Stack Developer in Mexico Axel Laurent Obscura Sarzotti"
+              className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
+            />
+            <div className="flex flex-col justify-between p-2 leading-normal">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">CODE ACADEMY</p>
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">FULL-STACK ENGINEER PROGRAM</h5>
+                <p className="text-sm">IN PROCESS: 2021 - 2022</p>
+            </div>
+          </Link>
           {/*
           <hr />
           <h2 className="p-2 font-bold">COURSES</h2>
